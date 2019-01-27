1d92b58fd6a72944033851be5925278138b60607
Andy Waite <andyw8@users.noreply.github.com>
2019-01-27 16:50:53 -0500

    Adjust directory structure

---
+++ lib/rubocop/cop/mocha_to_rspec.rb
@@ -0,0 +1,15 @@
+#require "rubocop"
+
+# require "rubocop/cop/mocha_to_rspec/version"
+require "rubocop/cop/mocha_to_rspec/and_return"
+require "rubocop/cop/mocha_to_rspec/any_instance"
+require "rubocop/cop/mocha_to_rspec/mock"
+require "rubocop/cop/mocha_to_rspec/stub"
+require "rubocop/cop/mocha_to_rspec/stubs_with_args"
+require "rubocop/cop/mocha_to_rspec/stubs_with_hash"
+require "rubocop/cop/mocha_to_rspec/stubs_with_no_args"
+require "rubocop/cop/mocha_to_rspec/stubs_with_no_return"
+
+module MochaToRspec
+  # Your code goes here...
+end
