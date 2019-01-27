0486b17369adb6bacae14f17d1b63bc0d04bb451
Andy Waite <andyw8@users.noreply.github.com>
2019-01-27 17:49:02 -0500

    Add cop_list method

--- lib/mocha_to_rspec.rb
+++ lib/mocha_to_rspec.rb
@@ -9,7 +9,17 @@ require "rubocop/cop/mocha_to_rspec/stubs_with_args"
 require "rubocop/cop/mocha_to_rspec/stubs_with_hash"
 require "rubocop/cop/mocha_to_rspec/stubs_with_no_args"
 require "rubocop/cop/mocha_to_rspec/stubs_with_no_return"
+require "active_support/core_ext/string/inflections" # for `camelize`
 
 module MochaToRSpec
   # Your code goes here...
+  def self.cop_list
+    Dir["lib/rubocop/cop/mocha_to_rspec/*"].map do |filename|
+      filename
+        .split("/")
+        .last
+        .gsub(".rb", '')
+        .camelize
+    end.map { |x| "#{MochaToRSpec}/#{x}" }
+  end
 end
