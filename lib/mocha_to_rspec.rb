af4fb7120b9a4613212542b841d3f4abef4c790a
Andy Waite <andyw8@users.noreply.github.com>
2019-01-31 22:29:39 -0500

    Remove AddReturn

--- lib/mocha_to_rspec.rb
+++ lib/mocha_to_rspec.rb
@@ -1,7 +1,6 @@
 require "rubocop"
 
 require "rubocop/cop/mocha_to_rspec/version"
-require "rubocop/cop/mocha_to_rspec/and_return"
 require "rubocop/cop/mocha_to_rspec/any_instance"
 require "rubocop/cop/mocha_to_rspec/mock"
 require "rubocop/cop/mocha_to_rspec/raises"
