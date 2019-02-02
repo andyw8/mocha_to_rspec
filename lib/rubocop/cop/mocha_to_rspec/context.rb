a8e4ce14a0a9de2ec422f23d244cb6e04ba284fb
Andy Waite <andyw8@users.noreply.github.com>
2019-02-01 20:51:45 -0500

    Fix

--- lib/rubocop/cop/mocha_to_rspec/context.rb
+++ lib/rubocop/cop/mocha_to_rspec/context.rb
@@ -6,16 +6,18 @@ module RuboCop
 
           # $(send (send nil? ...) :stubs ...)
         def_node_matcher :candidate?, <<-CODE
-          (send _ {:stubs :expects} ...)
+          $(send _ {:stubs :expects} _)
 CODE
 
         def on_send(node)
           candidate?(node) do
-            return unless (node.parent&.source&.include?("before(:all)") == true) ||
+            # require 'pry'; binding.pry
+            if (node.parent&.source&.include?("before(:all)") == true) ||
               (node.parent&.parent&.source&.include?("before(:all)") == true) ||
               (node.parent&.source&.include?("before(:context)") == true) ||
               (node.parent&.parent&.source&.include?("before(:context)") == true)
-            add_offense(node, location: :selector)
+              add_offense(node, location: :selector)
+            end
           end
         end
       end
